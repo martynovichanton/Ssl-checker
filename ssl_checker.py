0cdacb1c4e350bdf5786ea3f6a677dd365412f53
Martynovich Anton <martynovichanton@gmail.com>
2024-12-14 19:13:32 -0500

    update get result

--- ssl_checker.py
+++ ssl_checker.py
@@ -13,6 +13,20 @@ WORKERS_COUNT = multiprocessing.cpu_count()
 SOCKET_CONNECTION_TIMEOUT = 2
 DAYS_THRESHOLD = 30
 
+
+def get_result(result_func, log_file, host, context=False):
+    # future.result() if RUN_MULTITHREADING = True
+    # get_certificate_time(context, host) if RUN_MULTITHREADING = False
+    try:
+        result = result_func(context, host) if context else result_func()
+        days_remaining, time_remaining_txt, date, ip = result.values()
+        line = f"{host} {ip} {'WARN' if days_remaining < DAYS_THRESHOLD else 'OK'} {time_remaining_txt} {date}"
+        print(line)
+        log_file.write(f'{line}\n')
+    except Exception as e:
+        print(f'{host} ERROR {e}')
+        log_file.write(print(f'{host} ERROR {e}\n'))
+
 def get_certificate_time(context, host):
     h = host.split(":")[0]
     p = host.split(":")[1]
@@ -59,7 +73,7 @@ def check_certificates_all(hostnames_filename):
     else: 
         for host in hostnames:
             try:
-                result = future.result()
+                result = get_certificate_time(context, host)
                 days_remaining, time_remaining_txt, date, ip = result.values()
                 line = f"{host} {ip} {'WARN' if days_remaining < DAYS_THRESHOLD else 'OK'} {time_remaining_txt} {date}"
                 print(line)
@@ -67,6 +81,21 @@ def check_certificates_all(hostnames_filename):
             except Exception as e:
                 print(f'{host} ERROR {e}')
                 log_file.write(print(f'{host} ERROR {e}\n'))
+
+    #######################
+    #######################
+    #######################
+
+    # if RUN_MULTITHREADING:
+    #     with ThreadPoolExecutor(max_workers=WORKERS_COUNT) as executor:
+    #         future_list = {executor.submit(get_certificate_time, context, host): host for host in hostnames}
+    #         # print(future_list)
+    #         for future in as_completed(future_list):
+    #             host = future_list[future]
+    #             get_result(future.result, log_file, host)
+    # else: 
+    #     for host in hostnames:
+    #         get_result(get_certificate_time, log_file, host, context)
 
 
 def pluralise(singular, count):
